9b90f265628ada4ebffe42ed19730a628a1d584f
Yogeswaran <Yogeswaran.virubatchan@rakbank.ae>
2022-09-21 14:48:47 +0530

    basic_layout

---
+++ src/components/layout/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Layout } from "./Layout";
+
+const AppLayout = () => {
+    return (
+    <Layout/>       
+    )
+}
+
+export default AppLayout;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import * as serviceWorker from "./serviceWorker";
+import { App } from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<div>Hi</div>);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
